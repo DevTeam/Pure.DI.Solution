bc187261c343888aab95c2b479258c09d431c4fe
Nikolay Pianikov <nikolayp@mail.ru>
2024-08-06 21:23:09 +0300

    Update to 2.1.29

--- App/Composition.cs
+++ App/Composition.cs
@@ -7,5 +7,5 @@ internal partial class Composition
     private static void Setup() => DI.Setup()
             // Based on the Composition setup from the Lib namespace
             .DependsOn("Lib.Composition")
-            .Root<Program>("Root");
+            .Root<Program>(nameof(Root));
 }
